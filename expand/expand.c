1a75cf99493a1f9e4d11bc97c7936063ccd5eb6e
ppp-pp-pp <57984390+ppp-pp-pp@users.noreply.github.com>
2020-05-08 14:02:29 +0700

    expand

--- expand/expand.c
+++ expand/expand.c
@@ -1,1 +1,24 @@
-
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+int main(int argc, char **argv){
+	if(argc != 2){
+		printf("Jumlah argumen tidak sesuai.\n");
+		exit();
+	}
+	int fp = open(1, argv[1], O_RDONLY);
+	if(fp < 0){
+		printf(1, "%s tidak ditemukan.\n", argv[1]);
+		exit();
+	}
+	char buf[1000];
+	read(fp, buf, sizeof(buf));
+	int lf = strlen(buf);
+	int k;
+	for(k=0;k<lf;k++){
+		if(buf[k] != '\t')printf(1, "%c", buf[k]);
+		else printf(1, "        ");
+	}
+	exit();
+}
